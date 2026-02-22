3174eac7b167a300b1d900ae392d43462218e93b
ghadiz <geeziadeh@gmail.com>
2026-02-22 12:27:06 +0000

    fix test context

--- src/test/java/com/ghadyz/payflow/PayflowApplicationTests.java
+++ src/test/java/com/ghadyz/payflow/PayflowApplicationTests.java
@@ -2,8 +2,10 @@ package com.ghadyz.payflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PayflowApplicationTests {
 
 	@Test
